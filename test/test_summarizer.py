9e99091b74684d0c119cd07188d2d654b356627e
hariesramdhani <hydrolizedmaltose@gmail.com>
2020-01-31 17:54:45 +0700

    add test_summarizer.py

---
+++ test/test_summarizer.py
@@ -0,0 +1,40 @@
+import unittest
+import glob
+
+import pandas as pd
+
+from gurih.data.summarizer import Summarizer
+
+class SummarizerTest(unittest.TestCase):
+    """
+    Test if the summarizer returns the right outputs
+    """
+
+    def setUp(self):
+        self.dir = "test_data/test_summarizer"
+        self.X = glob.glob(f"{self.dir}/*.json")
+        self.summarizer = Summarizer(output_dir=self.dir)
+
+    def test_summarizer_output(self):
+        """
+        Test if Summarizer returns the correct output.
+        """
+        summarized_df = self.summarizer.transform(self.X)
+
+
+        # Check whether Summarizer correctly writes the output
+        csv_output = f"{self.dir}/statistical_summary.csv"
+        csvs = glob.glob(f"{self.dir}/*.csv")
+
+        print(csv_output)
+        print(csvs)
+
+        self.assertEqual(type(summarized_df), pd.DataFrame)
+        self.assertEqual(summarized_df.shape[1], 8)
+        self.assertGreater(summarized_df.shape[0], 1)
+        self.assertTrue(csv_output in csvs)
+
+if __name__ == "__main__":
+    unittest.main()
+
+        
