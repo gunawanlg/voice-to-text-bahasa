fadaab75dc1f67ef5d406cee9e6e298afdf3332f
Gunawan Lumban Gaol (ID) <gunimarbun@gmail.com>
2020-02-28 14:14:43 +0700

    add custom layers
    ASREncoder
    BahdanauAttention
    ASRDecoder.

--- gurih/models/model.py
+++ gurih/models/model.py
@@ -569,18 +569,3 @@ class Seq2SeqModel(_BaseModel):
     def __init__(self, input_shape, vocab_len, latent_dim=256, **kwargs):
         super().__init__(input_shape, vocab_len, **kwargs)
         self.latent_dim = latent_dim
-
-    def _create(self):
-        # Encoder parts
-        encoder_inputs = Input(shape=(None, self.vocab_len))
-        encoder = LSTM(self.latent_dim, return_state=True)
-        encoder_outputs, state_h, state_c = encoder(encoder_inputs)
-        encoder_states = [state_h, state_c]
-
-        decoder_inputs = Input(shape=(None, self.vocab_len))
-        decoder = LSTM(self.latent_dim, return_sequences=True, return_state=True)
-        decoder_outputs, _, _ = decoder(decoder_inputs, initial_states=encoder_states)
-        decoder_outputs = Dense(self.vocab, activation='softmax')(decoder_outputs)
-
-        model = Model([encoder_inputs, decoder_inputs], decoder_outputs)
-        pass
--- gurih/models/my_keras_layers.py
+++ gurih/models/my_keras_layers.py
@@ -2,11 +2,11 @@
 # KERAS CUSTOM LAYER COMPONENT #
 ################################
 
-from tensorflow.keras import Model
+import tensorflow as tf
 from tensorflow.keras.layers import Conv1D, LSTM, Bidirectional, Dense
 
 
-class BaselineASR(Model):
+class BaselineASR(tf.keras.Model):
     def __init__(self, filters, kernel_size, strides, padding, n_lstm_units, n_dense_units):
         super(BaselineASR, self).__init__()
         self.conv1d        = Conv1D(filters,
@@ -29,3 +29,101 @@ class BaselineASR(Model):
         x = self.bilstm(x)
         x = self.dense(x)
         return x
+
+
+class ASREncoder(tf.keras.Model):
+    def __init__(self, vocab_len, n_lstm, batch_size):
+        super(ASREncoder, self).__init__()
+        self.vocab_len = vocab_len
+        self.n_lstm = n_lstm
+        self.batch_size = batch_size
+
+        self.bilstm = Bidirectional(LSTM(self.n_lstm,
+                                         return_sequences=True,
+                                         return_state=True,
+                                         recurrent_initializer='orthogonal'))  # or 'glorot_uniform'
+
+    def call(self, X, hidden):
+        """
+        Parameters
+        ----------
+        X : shaoe=(m, Tx, n_mfcc)
+            audio sequence input
+
+        Returns
+        -------
+        output : shape=(m, Tx, n_lstm)
+            output from lSTM
+        state : shape=(m, n_lstm)
+            final LSTM state
+        """
+        output, state = self.bilstm(X, initial_state=hidden)
+        return output, state
+
+    def initialize_hidden_state(self):
+        return tf.zeros((self.batch_size, self.n_lstm))
+
+
+class BahdanauAttention(tf.keras.Model):
+    def __init__(self, n_dense):
+        super(BahdanauAttention, self).__init__()
+        self.n_dense = n_dense
+        self.W1 = Dense(n_dense)  # no activation g(x) = x
+        self.W2 = Dense(n_dense)  # no activation g(x) = x
+        self.V = Dense(1)  # no activation g(x) = x
+
+    def call(self, query, values):
+        # query hidden state shape == (batch_size, hidden size)
+        # query_with_time_axis shape == (batch_size, 1, hidden size)
+        # values shape == (batch_size, max_len, hidden size)
+        # we are doing this to broadcast addition along the time axis to calculate the score
+        query_with_time_axis = tf.expand_dims(query, 1)
+
+        # score shape == (batch_size, max_length, 1)
+        # we get 1 at the last axis because we are applying score to self.V
+        # the shape of the tensor before applying self.V is (batch_size, max_length, units)
+        score = self.V(tf.nn.tanh(
+            self.W1(query_with_time_axis) + self.W2(values)))
+
+        # attention_weights shape == (batch_size, max_length, 1)
+        attention_weights = tf.nn.softmax(score, axis=1)
+
+        # context_vector shape after sum == (batch_size, hidden_size)
+        context_vector = attention_weights * values
+        context_vector = tf.reduce_sum(context_vector, axis=1)
+
+        return context_vector, attention_weights
+
+
+class ASRDecoder(tf.keras.Model):
+    def __init__(self, vocab_len, n_lstm, n_units, batch_size):
+        super(ASRDecoder, self).__init__()
+        self.vocab_len = vocab_len
+        self.n_lstm = n_lstm
+        self.n_units = n_units
+        self.batch_size = batch_size
+
+        self.lstm = LSTM(self.n_lstm,
+                         return_sequences=True,
+                         return_states=True,
+                         recurrent_initializer='orthogonal')  # or 'glorot_uniform'
+        self.fc = Dense(self.vocab_len)
+        self.attention = BahdanauAttention(self.n_units)
+
+    def call(self, X, hidden, enc_output):
+        """
+        Parameters
+        ----------
+
+        """
+        context_vector, attention_weights = self.attention(hidden, enc_output)
+
+        X = tf.expand_dims(X, 1)
+        X = tf.concat([tf.expand_dims(context_vector, 1), X], axis=-1)
+
+        output, state = self.LSTM(X)
+
+        output = tf.reshape(output, (-1, output.shape[2]))
+        X = self.fc(output)
+
+        return X, state, attention_weights
